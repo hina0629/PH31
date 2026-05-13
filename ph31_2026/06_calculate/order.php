355cbb6ce913234e2696c9e91ca7efe3e46a4d2d
hina0629 <hina.0629.xxx@gmail.com>
2026-05-13 15:48:56 +0900

    TODO: テンプレートリテラル

--- ph31_2026/06_calculate/order.php
+++ ph31_2026/06_calculate/order.php
@@ -9,7 +9,8 @@ $full_name = $last_name . " " . $first_name;
 // pythonとかだと、
 // full_name = last_name + " " + first_name
 // TODO: テンプレートリテラル: last_name と first_name の間に半角スペースを入れる
-$full_name = "";
+$full_name = "$last_name $first_name";
+// $full_name = "{$last_name} {$first_name}" でもOK
 
 // drink1, drink2, drink3に商品名を代入
 $drink1 = "コーラ";
