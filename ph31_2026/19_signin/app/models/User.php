02f9d7e9646dff370ccbfa81f77bcc849e5f81b7
hina0629 <hina.0629.xxx@gmail.com>
2026-06-17 16:26:48 +0900

    TODO: パスワード検証して、$user を返す

--- ph31_2026/19_signin/app/models/User.php
+++ ph31_2026/19_signin/app/models/User.php
@@ -140,9 +140,12 @@ class User extends Model
             $stmt = $pdo->prepare($sql);
             // SQL実行
             $stmt->execute(['account_name' => $account_name]);
-            // 結果取得
+            // 結果取得: 1件取得
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
             // TODO: パスワード検証して、$user を返す
+            if ($user && password_verify($password, $user['password'])) {
+                return $user;
+            }
             return null;
         } catch (PDOException $e) {
             error_log($e->getMessage());
