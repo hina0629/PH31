f13c1bab2fb4a08d76af7bcda18cee1751034b8b
hina0629 <hina.0629.xxx@gmail.com>
2026-07-01 15:47:07 +0900

    TODO: いいねを追加するSQL文

--- ph31_2026/20_php_sns/app/models/Like.php
+++ ph31_2026/20_php_sns/app/models/Like.php
@@ -43,7 +43,7 @@ class Like
             // DB接続
             $pdo = Database::getInstance();
             // TODO: いいねを追加するSQL文: user_id と tweet_id を指定
-            $sql = "";
+            $sql = "INSERT INTO likes (tweet_id, user_id) VALUES (:tweet_id, :user_id)";
             // SQL事前準備
             $stmt = $pdo->prepare($sql);
             // SQL実行
